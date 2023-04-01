0f682536689debc22dcec360f18fe94084fcfe6c
doongjohn <doongjohn@gmail.com>
2023-04-01 15:07:43 +0900

    update

--- cmake/fsm.cmake
+++ cmake/fsm.cmake
@@ -3,57 +3,43 @@ list(REMOVE_ITEM SRC_LUA
   ${PROJECT_SOURCE_DIR}/vendor/lua-5.4.4/src/lua.c
   ${PROJECT_SOURCE_DIR}/vendor/lua-5.4.4/src/luac.c)
 
-# dynamic library
+# object file
+# https://stackoverflow.com/a/68406028
 file(GLOB_RECURSE SRC_FSM src/fsm/*.cpp)
-add_library(cpp_fsm SHARED
+add_library(cpp_fsm_obj OBJECT
   ${SRC_LUA}
   ${SRC_FSM})
 
-target_compile_features(cpp_fsm
-  PRIVATE cxx_std_20)
+target_compile_features(cpp_fsm_obj
+  PUBLIC cxx_std_20)
 
-target_compile_options(cpp_fsm
-  PRIVATE
+target_compile_options(cpp_fsm_obj
+  PUBLIC
+  -fPIC # https://stackoverflow.com/a/31509007
   -Wall
   $<$<CONFIG:Release>:-flto>
   ${SANITIZER_OPTIONS})
 
-target_link_options(cpp_fsm
-  PRIVATE
+target_link_options(cpp_fsm_obj
+  PUBLIC
+  -fPIC
   -Wall
   $<$<CONFIG:Release>:-flto>
   ${SANITIZER_OPTIONS})
 
-target_include_directories(cpp_fsm
-  PRIVATE ${PROJECT_SOURCE_DIR}/vendor/lua-5.4.4/src
-  PRIVATE ${PROJECT_SOURCE_DIR}/vendor)
+target_include_directories(cpp_fsm_obj
+  PUBLIC ${PROJECT_SOURCE_DIR}/vendor/lua-5.4.4/src
+  PUBLIC ${PROJECT_SOURCE_DIR}/vendor)
 
-target_compile_definitions(cpp_fsm
-  PRIVATE SOL_ALL_SAFETIES_ON=1)
+target_compile_definitions(cpp_fsm_obj
+  PUBLIC SOL_ALL_SAFETIES_ON=1)
+
+# dynamic library
+add_library(cpp_fsm SHARED)
+target_link_libraries(cpp_fsm
+  PRIVATE cpp_fsm_obj)
 
 # static library
-add_library(cpp_fsm_static
-  ${SRC_LUA}
-  ${SRC_FSM})
-
-target_compile_features(cpp_fsm_static
-  PRIVATE cxx_std_20)
-
-target_compile_options(cpp_fsm_static
-  PRIVATE
-  -Wall
-  $<$<CONFIG:Release>:-flto>
-  ${SANITIZER_OPTIONS})
-
-target_link_options(cpp_fsm_static
-  PRIVATE
-  -Wall
-  $<$<CONFIG:Release>:-flto>
-  ${SANITIZER_OPTIONS})
-
-target_include_directories(cpp_fsm_static
-  PRIVATE ${PROJECT_SOURCE_DIR}/vendor/lua-5.4.4/src
-  PRIVATE ${PROJECT_SOURCE_DIR}/vendor)
-
-target_compile_definitions(cpp_fsm_static
-  PRIVATE SOL_ALL_SAFETIES_ON=1)
+add_library(cpp_fsm_static STATIC)
+target_link_libraries(cpp_fsm_static
+  PRIVATE cpp_fsm_obj)
