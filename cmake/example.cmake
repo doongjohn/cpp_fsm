c49cb6a97e15029061db5945ae9d98a5ade7f83f
doongjohn <doongjohn@gmail.com>
2023-04-01 16:45:46 +0900

    update

--- cmake/example.cmake
+++ cmake/example.cmake
@@ -1,12 +1,28 @@
+# backward (stack trace printer)
+# https://github.com/bombela/backward-cpp
+if (UNIX AND NOT APPLE)
+  add_library(backward STATIC
+    vendor/backward/backward.cpp)
+
+  target_compile_features(backward
+    PRIVATE cxx_std_20)
+
+  target_compile_options(backward
+    PRIVATE ${SANITIZER_OPTIONS})
+
+  target_link_options(backward
+    PRIVATE ${SANITIZER_OPTIONS})
+
+  target_compile_definitions(backward
+    PRIVATE BACKWARD_HAS_DW=1)
+endif()
+
 # example
-file(GLOB_RECURSE SRC_EXAMPLE
-  vendor/backward/*.cpp # https://github.com/bombela/backward-cpp
-  src/example/*.cpp)
-add_executable(example
-  ${SRC_EXAMPLE})
+file(GLOB_RECURSE SRC_EXAMPLE src/example/*.cpp)
+add_executable(example ${SRC_EXAMPLE})
 
 # cmake --build build -v --config Debug --target example
-set_target_properties(example PROPERTIES EXCLUDE_FROM_ALL TRUE)
+set_target_properties(example PROPERTIES EXCLUDE_FROM_ALL true)
 
 target_compile_features(example
   PRIVATE cxx_std_20)
@@ -28,9 +44,12 @@ target_include_directories(example
   PRIVATE ${PROJECT_SOURCE_DIR}/vendor)
 
 target_compile_definitions(example
-  PRIVATE SOL_ALL_SAFETIES_ON=1
-  PRIVATE BACKWARD_HAS_DW=1)
+  PRIVATE SOL_ALL_SAFETIES_ON=1)
 
 target_link_libraries(example
-  PRIVATE dw
   PRIVATE cpp_fsm_shared)
+
+if (UNIX AND NOT APPLE)
+  target_link_libraries(example
+    PRIVATE backward)
+endif()
