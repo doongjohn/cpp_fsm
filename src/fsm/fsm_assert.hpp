d8d9cb22dc1aa079f9b7b60ff5fb28b81f965667
doongjohn <doongjohn@gmail.com>
2023-04-03 10:53:19 +0900

    update

--- src/fsm/fsm_assert.hpp
+++ src/fsm/fsm_assert.hpp
@@ -8,10 +8,21 @@
 
 namespace LDJ {
 
+struct FsmAssert {
+  static std::function<void(bool)> fn_assert;
+};
+
+inline std::function<void(bool)> FsmAssert::fn_assert = nullptr;
+
 inline auto fsm_assert_msg(bool expr, std::string msg) -> void {
   if (!expr)
     fsm_log("Assertion failed: " + msg);
-  assert(expr);
+
+  if (FsmAssert::fn_assert) {
+    FsmAssert::fn_assert(expr);
+  } else {
+    assert(expr);
+  }
 }
 
 } // namespace LDJ
--- src/fsm/fsm_logger.hpp
+++ src/fsm/fsm_logger.hpp
@@ -7,14 +7,14 @@
 namespace LDJ {
 
 struct FsmLogger {
-  static std::function<void(std::string, std::string)> logger;
+  static std::function<void(std::string, std::string)> fn_logger;
 };
 
-inline std::function<void(std::string, std::string)> FsmLogger::logger = nullptr;
+inline std::function<void(std::string, std::string)> FsmLogger::fn_logger = nullptr;
 
 inline auto fsm_log(std::string msg, std::string prefix = "[FSM] ") -> void {
-  if (FsmLogger::logger) {
-    FsmLogger::logger(msg, prefix);
+  if (FsmLogger::fn_logger) {
+    FsmLogger::fn_logger(msg, prefix);
   } else {
     std::cout << prefix << msg << '\n';
   }
