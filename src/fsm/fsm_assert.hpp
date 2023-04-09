bc863318b1e71fd1388234467d35e3b25bdbadf2
doongjohn <doongjohn@gmail.com>
2023-04-09 14:30:13 +0900

    update

--- src/fsm/fsm_assert.hpp
+++ src/fsm/fsm_assert.hpp
@@ -8,15 +8,14 @@
 namespace LDJ {
 
 struct FsmAssert {
-  inline static std::function<void(bool)> fn_assert = nullptr;
+  inline static std::function<void(bool, std::string)> fn_assert = nullptr;
 };
 
 inline auto fsm_assert_msg(bool expr, const std::string &msg) -> void {
   if (!expr) {
     fsm_log("Assertion failed: " + msg);
-
     if (FsmAssert::fn_assert) {
-      FsmAssert::fn_assert(expr);
+      FsmAssert::fn_assert(expr, msg);
     } else {
       throw msg;
     }
